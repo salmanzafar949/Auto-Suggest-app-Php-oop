29ee5a691acd754371b29da25f868f8fcb9c9a76
Salman Zafar <salmanzafar949@gmail.com>
2017-11-21 09:51:36 +0500

    Added Cdns

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by Salman Zafar.
+ * User: sam
+ * Date: 11/21/17
+ * Time: 9:49 AM
+ */
+
---
+++ scripts.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by Salman Zafar.
+ * User: sam
+ * Date: 11/21/17
+ * Time: 9:51 AM
+ */
+
+?>
+
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
